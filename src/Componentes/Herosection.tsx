8a5b252380bd01a62abc7ac64dcf63b5f31448d3
Rosibel Campos <rosibelcampos85@gmail.com>
2025-07-06 11:12:38 -0400

    modificacion en Herosection

--- src/Componentes/Herosection.tsx
+++ src/Componentes/Herosection.tsx
@@ -1,11 +1,33 @@
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'; 
-import curriculum from '../curriculum.json' with { type: "json" };
-const {conceptosBasicos} = curriculum;
-const {imagen,nombre, email, perfiles, resumen } = conceptosBasicos;
+import{ useEffect, useState } from 'react';
+import type { InterfaceHerosection } from '../Interface/InterfaceHerosection.tsx';
+//import curriculum from '../curriculum.json' with { type: "json" };
+import axios from 'axios';
+//const {conceptosBasicos} = curriculum;
+//const {imagen,nombre, email, perfiles, resumen } = conceptosBasicos;
 const TextColor = '#ffffff'; // White
 const AccentColor = '#40e0d0'; // Turquoise (light)
 
 function Herosection() {
+
+    interface estados{
+        herosection:InterfaceHerosection[];
+    }
+
+    const [info, setInfo] = useState<estados['herosection']>([]);
+
+    const infoData = async () => {
+        const response = await axios.get("../curriculum.json");
+        const {conceptosBasicos} = response.data;
+        setInfo(conceptosBasicos);
+    }
+
+    useEffect(() => {
+        infoData();
+    });
+
+    info.map(conceptosBasicos => {
+        const {imagen,nombre, email, perfiles, resumen } = conceptosBasicos;
 
   return (
   <div>
@@ -28,13 +50,13 @@ function Herosection() {
                     <a href={`mailto:${email}`} style={{ color: AccentColor, textDecoration: 'none' }}>
                         <FaEnvelope size={24} /> {email}
                     </a>
-                    {perfiles && perfiles[1] && (
-                      <a href={perfiles[1].url} target="_blank" rel="noopener noreferrer" style={{ color: AccentColor }}>
+                    {perfiles && perfiles[0] && (
+                      <a href={perfiles[0].url} target="_blank" rel="noopener noreferrer" style={{ color: AccentColor }}>
                           <FaGithub size={24} />
                       </a>
                     )}
-                    {perfiles && perfiles[0] && (
-                      <a href={perfiles[0].url} target="_blank" rel="noopener noreferrer" style={{ color: AccentColor }}>
+                    {perfiles && perfiles[1] && (
+                      <a href={perfiles[1].url} target="_blank" rel="noopener noreferrer" style={{ color: AccentColor }}>
                           <FaLinkedin size={24} />
                       </a>
                     )}
@@ -46,8 +68,9 @@ function Herosection() {
         </section>
 
   </div>
-
-  )
+  
+)
+});
 }
 
 export default Herosection;
